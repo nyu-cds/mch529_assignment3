eb9e2a8fb73aad57d76572929aebfdff756d0d77
Michael Higgins <mch529@nyu.edu>
2017-04-30 23:15:27 -0400

    add comments, fix rounding error

--- mandelbrot_gpu.py
+++ mandelbrot_gpu.py
@@ -42,20 +42,22 @@ def compute_mandel(min_x, max_x, min_y, max_y, image, iters  ):
     
     pixel_size_x = (max_x - min_x) / width
     pixel_size_y = (max_y - min_y) / height
-                   
+    
+    #get block absolute indexes 
     block_id_x, block_id_y = cuda.grid(2)
     
     num_block_x = block_dim[0]*grid_dim[0]
     num_block_y = block_dim[1]*grid_dim[1]
     
-    pixels_per_block_x = math.floor(width/(1.0*num_block_x))
-    pixels_per_block_y = math.floor(height/(1.0*num_block_y))
+    #divide blocks evenly among pixels
+    pixels_per_block_x = math.ceil(width/(1.0*num_block_x))
+    pixels_per_block_y = math.ceil(height/(1.0*num_block_y))
 
-    #print(pixels_per_block_x,pixels_per_block_y)
-
+    #find upper lhs of relative pixel (0,0) for this block
     start_pixel_x = block_id_x * pixels_per_block_x
     start_pixel_y = block_id_y * pixels_per_block_y
     
+    # only calculate for this block
     pixels_x = range(start_pixel_x,start_pixel_x + pixels_per_block_x )
     pixels_y = range(start_pixel_y,start_pixel_y + pixels_per_block_y )
 
@@ -64,20 +66,19 @@ def compute_mandel(min_x, max_x, min_y, max_y, image, iters  ):
         real = min_x + i*pixel_size_x
         for j in pixels_y:
             imag =  min_y + j*pixel_size_y
-            #image[i,j]= 100
-            #if j<height and i < width:
-            #image[j,i]= mandel(real,imag, iters) 
+            if j<height and i < width:
+                image[j,i]= mandel(real,imag, iters) 
             
 if __name__ == '__main__':
     image_w, image_h =1024,1536
     
-    block_dim = (32, 8)
+    block_dim = (32, 16)
     grid_dim = (32, 16)
 
     image = np.zeros((image_w, image_h), dtype = np.uint8)
     
     image_global_mem = cuda.to_device(image)
-    compute_mandel[grid_dim, block_dim](-2.0, 1.0, -1.0, 1.0, image_global_mem, 10 )
+    compute_mandel[block_dim, grid_dim](-2.0, 1.0, -1.0, 1.0, image_global_mem, 30 )
     image_global_mem.copy_to_host()
     imshow(image)
     show()
